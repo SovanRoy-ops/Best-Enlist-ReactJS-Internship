26a39532617e1f9b1538f2b5a888291a504b3c3d
SovanRoy-ops <75088235+SovanRoy-ops@users.noreply.github.com>
2020-11-27 21:58:28 +0530

    day1 tasks added

---
+++ Day1/Task_3.js
@@ -0,0 +1,5 @@
+
+// Write a JavaScript program to check the length of the string
+
+var str = "Hello World!";
+console.log(str.length);
---
+++ Day1/dist/Task_2.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50 
+var x = 30;
+var y = 50;
+console.log(x == 50 || y == 50 || x + y == 50);
